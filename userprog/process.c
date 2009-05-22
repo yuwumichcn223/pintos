e69c8095bf160567615a275562c961016ee7a052
ChrisKXu <XuHotdogs@gmail.com>
2009-05-23 02:42:48 +0800

    66/76 on pintos, tagged args_freeze

--- userprog/process.c
+++ userprog/process.c
@@ -93,6 +93,10 @@ start_process (void *file_name_)
               palloc_free_page (argv_off);
               thread_exit ();
             }
+          while (*(save_ptr) == ' ')
+            {
+              ++save_ptr;
+            }
           argv_off[++argc] = save_ptr - file_name;
         }
   /* == My Implementation */
